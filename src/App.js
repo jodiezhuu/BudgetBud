5a4593624802a96e3620b7c6d29169bc3f08270f
Jeffrey <348627894@gapps.yrdsb.ca>
2021-06-12 13:42:45 -0400

    added info section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Info from './components/Info';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Info />
       <Switch>
         <Route path='/' />
       </Switch>
--- src/components/info.js
+++ src/components/info.js
@@ -1,21 +1,42 @@
+import './Info.css';
+
 const Info = () =>  {
+
+	const onClick = () => {
+		
+	}
+	
 	return (
-		<div classname = "infoSection">
+		<div className="infoSection">
 			<form>
+				<p className = "uNameTitle">Name</p>
 				<input type="text" id = "username" name = "uName"/>
+				<p>Salary</p>
 				<input type="text" id = "salary" name = "uMoney"/>
+				<p>Tax</p>
 				<input type="text" id = "tax" name = "uTax"/>
+				<p>Phone</p>
 				<input type="text" id = "phoneBill" name = "uPhone"/>
+				<p>Electricity</p>
 				<input type="text" id = "electricity" name = "uElec"/>
+				<p>Rent</p>
 				<input type="text" id = "rent" name = "uRent"/>
+				<p>Internet</p>
 				<input type="text" id = "internet" name = "uNet"/>
+				<p>Insurance</p>
 				<input type="text" id = "insurance" name = "uInsure"/>
+				<p>Car</p>
 				<input type="text" id = "carpay" name = "uCar"/>
+				<p>Mortgage</p>
 				<input type="text" id = "mortgage" name = "uMort"/>
+				<p>Water</p>
 				<input type="text" id = "water" name = "uWater"/>
+				<p>Subscriptions</p>
 				<input type="text" id = "subs" name = "uSub"/>
+				<p>Entertainment</p>
 				<input type="text" id = "entertainment" name = "uFun"/>
 			</form>
+			<button type="button" onClick={onClick}>Submit</button>
 		</div>
 	)
 }
