b51a0c58cb679e0be455e9ba3a89522dfec623e4
jodiezhuu <jodiezhu12@gmail.com>
2021-06-12 19:32:46 -0400

    homepage

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ const Intro = () =>  {
 	
 	return (
         <div>
-            <h1 className='homepage'>Budgeting made easy!
+            <h1 className='homepage'>Budget planning, made easy!
             <h2 className='description'> BudgetBud is a budget tracker for your personal finances! This financing program allows you to keep track of your expenditures, assets, and savings.</h2>
             </h1>
         </div>
