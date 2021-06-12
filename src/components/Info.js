e2f601dc8b3f8a6f40d52bc5dea470931c531075
Heshanth Rasalingam <heshanth@Heshanths-MacBook-Pro.local>
2021-06-12 16:12:13 -0400

    Form with validation

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,44 +1,59 @@
 import './Info.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Info = () =>  {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+  
+    console.log(watch("budgetBud.Salary"));
+  
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p>Name</p>
+        <input {...register("budgetBud.Name")} />
 
-	const onClick = () => {
-		
-	}
-	
-	return (
-		<div className="infoSection">
-			<form>
-				<p className = "uNameTitle">Name</p>
-				<input type="text" id = "username" name = "uName"/>
-				<p>Salary</p>
-				<input type="text" id = "salary" name = "uMoney"/>
-				<p>Tax</p>
-				<input type="text" id = "tax" name = "uTax"/>
-				<p>Phone</p>
-				<input type="text" id = "phoneBill" name = "uPhone"/>
-				<p>Electricity</p>
-				<input type="text" id = "electricity" name = "uElec"/>
-				<p>Rent</p>
-				<input type="text" id = "rent" name = "uRent"/>
-				<p>Internet</p>
-				<input type="text" id = "internet" name = "uNet"/>
-				<p>Insurance</p>
-				<input type="text" id = "insurance" name = "uInsure"/>
-				<p>Car</p>
-				<input type="text" id = "carpay" name = "uCar"/>
-				<p>Mortgage</p>
-				<input type="text" id = "mortgage" name = "uMort"/>
-				<p>Water</p>
-				<input type="text" id = "water" name = "uWater"/>
-				<p>Subscriptions</p>
-				<input type="text" id = "subs" name = "uSub"/>
-				<p>Entertainment</p>
-				<input type="text" id = "entertainment" name = "uFun"/>
-			</form>
-			<button type="button" onClick={onClick}>Submit</button>
-		</div>
-	)
+        <p>Salary</p>
+        <input {...register("budgetBud.Salary", {
+            required: true,
+            pattern: /^(0|[1-9][0-9]*)$/i
+        })} />
+        {errors.Salary?.type === "required" && <span>Oops! You forgot to enter your salary.</span>}
+        {errors.Salary?.type === "pattern" && <span>Make sure your salary is to the nearest dollar!</span>}
+
+        <p>Tax</p>
+        <input {...register("budgetBud.Tax")} />
+
+        <p>Phone</p>
+        <input {...register("budgetBud.Phone")} />
+
+        <p>Electricity</p>
+        <input {...register("budgetBud.Electricity")} />
+
+        <p>Rent</p>
+        <input {...register("budgetBud.Rent")} />
+
+        <p>Insurance</p>
+        <input {...register("budgetBud.Insurance")} />
+
+        <p>Car</p>
+        <input {...register("budgetBud.Car")} />
+
+        <p>Mortgage</p>
+        <input {...register("budgetBud.Mortgage")} />
+
+        <p>Water Bill</p>
+        <input {...register("budgetBud.WaterBill")} />
+
+        <p>Subscriptions</p>
+        <input {...register("budgetBud.Subscriptions")} />
+
+        <p>Entertainment</p>
+        <input {...register("budgetBud.Entertainment")} />
+
+        <input type="submit" />
+      </form>
+    );
 }
 
 export default Info
