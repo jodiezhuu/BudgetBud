b6622ddadbe156a8401396736d36f02485938b5b
jodiezhuu <jodiezhu12@gmail.com>
2021-06-12 16:12:55 -0400

    fix description statement

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,7 @@ const Intro = () =>  {
 	return (
         <div>
             <h1 className='homepage'>Budgeting made easy!
-            <h2 className='description'> BudgetBud is a budget tracker of your personal finances! This budget program allows you to keep track of your expenditures, assets, and savings.</h2>
+            <h2 className='description'> BudgetBud is a budget tracker for your personal finances! This financing program allows you to keep track of your expenditures, assets, and savings.</h2>
             </h1>
         </div>
 	)
