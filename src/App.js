064d65a048a4813469599cc2147c10964fa4793e
jodiezhuu <jodiezhu12@gmail.com>
2021-06-11 22:27:58 -0400

    website layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/'/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { GiMoneyStack } from 'react-icons/gi';
+import { FaMoneyCheck, FaRegMoneyBillAlt } from 'react-icons/fa';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to='/' className='navbar-logo'>
+                        <GiMoneyStack className='navbar-icon' />
+                        BudgetBuddy
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                    {click ? <FaMoneyCheck className='close' /> : <FaRegMoneyBillAlt className='close' />}
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li>
+                        <Link to='/' className='nav-links'>
+                            About Us
+                        </Link>
+                        </li>
+                        <li>
+                        <Link to='/' className='nav-links'>
+                            Personal Info
+                        </Link>
+                        </li>
+                        <li>
+                        <Link to='/' className='nav-links'>
+                            Calculated Expenditures
+                        </Link>
+                        </li>
+                        <li>
+                        <Link to='/' className='nav-links'>
+                            Calculated Savings
+                        </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar
