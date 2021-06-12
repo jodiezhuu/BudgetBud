0813a53f93afa6f8f6707b5c73c31beaf9a89dbf
jodiezhuu <jodiezhu12@gmail.com>
2021-06-12 16:07:35 -0400

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Intro from './components/Intro';
 import Info from './components/Info';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Intro />
       <Info />
       <Switch>
         <Route path='/' />
---
+++ src/components/Intro.js
@@ -0,0 +1,21 @@
+import './Intro.css';
+
+const Intro = () =>  {
+
+	const onClick = () => {
+		
+	}
+	
+	return (
+        <div>
+            <h1 className='homepage'>Budgeting made easy!
+            <h2 className='description'> BudgetBud is a budget tracker of your personal finances! This budget program allows you to keep track of your expenditures, assets, and savings.</h2>
+            </h1>
+        </div>
+	)
+}
+
+
+
+
+export default Intro
