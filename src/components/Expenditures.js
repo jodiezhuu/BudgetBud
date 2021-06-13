45718000915e4a33b7aaaddedf264d0443e61f92
heshanth <31254887+heshanth@users.noreply.github.com>
2021-06-13 09:21:52 -0400

    Expenditures JS with each category

--- src/components/Expenditures.js
+++ src/components/Expenditures.js
@@ -10,20 +10,17 @@ const Expenditures = () => {
     return (
         <>
             <div className="expenditures">
-                <h1 className='exptitle'>Total Spending Money</h1>
-                <h2 className='expcaption'>Here, you can see your spending money for the month.</h2>
-                <CircularProgressbar className="expprogressbar"
-                value={exp*3/4} 
-                text={`$${exp*3/4}`}
-                styles={buildStyles({
-                    textColor: '#8E8D8A',
-                    pathColor: '#ad9c84',
-                })
-                }
-			
-                />
+                <h1 className='exptitle'>Total Expenditure</h1>
+                <h2 className='expcaption'>Here, you can see your expenditures for the month.</h2>
+
+                <h2 className="expcaption">Recommended amount for entertainment: {0.15*exp}</h2>
+                <h2 className="expcaption">Recommended amount for clothes: {0.05*exp}</h2>
+                <h2 className="expcaption">Recommended amount for medical savings: {0.20*exp}</h2>
+                <h2 className="expcaption">Recommended amount for emergancy fund: {0.20*exp}</h2>
+                <h2 className="expcaption">Recommended amount for other spending: {0.40*exp}</h2>
+
                 <button onClick={() => setExp(
-			(document.getElementsByClassName("input")[0].value
+			(document.getElementsByClassName("input")[0].value - 
 			- document.getElementsByClassName("input")[1].value
 			- document.getElementsByClassName("input")[2].value
 			- document.getElementsByClassName("input")[3].value
@@ -31,7 +28,16 @@ const Expenditures = () => {
 			- document.getElementsByClassName("input")[5].value
 			- document.getElementsByClassName("input")[6].value
 			- document.getElementsByClassName("input")[7].value
-			- document.getElementsByClassName("input")[8].value)
+			- document.getElementsByClassName("input")[8].value) - 
+            ((document.getElementsByClassName("input")[0].value - 
+			- document.getElementsByClassName("input")[1].value
+			- document.getElementsByClassName("input")[2].value
+			- document.getElementsByClassName("input")[3].value
+			- document.getElementsByClassName("input")[4].value
+			- document.getElementsByClassName("input")[5].value
+			- document.getElementsByClassName("input")[6].value
+			- document.getElementsByClassName("input")[7].value
+			- document.getElementsByClassName("input")[8].value)/4)
 		)}>
                         Update Values
                 </button> 
